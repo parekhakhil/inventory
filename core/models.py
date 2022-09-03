276d57341b9bcdf8cf2136e79a9350bc6c115f0f
Akhil Parekh <parekhakhil11@gmail.com>
2022-09-04 01:42:27 +0530

    initialized core module

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BaseModel(models.Model):
+    created_by = models.CharField(max_length=100, blank=True, null=True)
+    updated_by = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        absract = True
